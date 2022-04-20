e120d3ea5dbd67816f4878d8076ff33d92149ed9
RavenDuffy <40807825+RavenDuffy@users.noreply.github.com>
2022-04-21 00:42:59 +0100

    added error log

--- ChatBot/src/cmds/fact.ts
+++ ChatBot/src/cmds/fact.ts
@@ -2,12 +2,14 @@ import Axios from 'axios'
 import { Command } from 'types/command'
 
 const randomFact: Command = (target, context, client) => {
-  Axios.get(`${process.env.API_URL!}/random-fact`).then((res) => {
-    if (res.data.error) return
-    else if (res.data.text) {
-      client.say(target, `${context['display-name']} ${res.data.text}`)
-    }
-  })
+  Axios.get(`${process.env.API_URL!}/random-fact`)
+    .then((res) => {
+      if (res.data.error) return
+      else if (res.data.text) {
+        client.say(target, `${context['display-name']} ${res.data.text}`)
+      }
+    })
+    .catch((error) => console.log(error))
 }
 
 const aliases = ['fact', 'random-fact', 'randomfact', 'rf']
